5b90f0e486ec16bbbaa54f182af1f9712971d890
vinitha <vinitha.nec@gmail.com>
2023-06-20 23:03:41 -0700

    Complete creating textbox for each image

--- frontend/src/components/features/Picture.jsx
+++ frontend/src/components/features/Picture.jsx
@@ -3,9 +3,15 @@ import { useEffect, useRef } from "react";
 import { useNavigate } from 'react-router-dom';
 
 
+
+
 function Picture(props){
    const cloudinaryRef = useRef();
    const widgetRef = useRef();
+//    let uploadedUrl;
+
+   const [showImage, setShowImage] = useState("");
+   const [imageUrls, setImageUrls] = useState([]);
 
    useEffect(cloudinaryValue, [])
 
@@ -15,9 +21,26 @@ function Picture(props){
         cloudName:'journalpictures',
         uploadPreset:'k2hycxis'
         }, function(error,result) {
-                console.log(result);
+                // console.log(result);
+                const url = result.info.url;
+                console.log(url);
+                if (url) {
+                    setShowImage(url);
+                    // uploadedUrl = url;
+                    // setstageparameters(previousstatevalue)
+                    setImageUrls(imageUrls => [...imageUrls, url])
+                    // setImageUrls(prevUrls => [...prevUrls, url]);
+                    // setImageUrls((prevUrls) => { return [...prevUrls, url] });
+                    // setImageUrls(preurls);
+                    console.log(showImage)};
+                
         });
-   }
+    }
+  
+    // function preurls(urls){
+
+    //     return [...urls, uploadedUrl];
+    // }
     function displayImage(){
             widgetRef.current.open()
         }
@@ -25,10 +48,20 @@ function Picture(props){
     return(
         <div>
             <p>This is where you will upload pictures</p>
-            <input type="file" name="file"/>
+            {/* <input type="file" name="file"/> */}
+            {/* <input type="text"/> */}
             {/* <button onClick={onButtonClick}></button> */}
-            <button type="submit">Upload</button>
-            <button onClick={displayImage}/>Cloudinary
+            {/* <button type="submit">Upload</button> */}
+            <button onClick={displayImage}>Cloudinary</button>
+            <div style={{ marginLeft: "50px", marginTop: "50px"}}/>
+            {imageUrls.map((url, index) => (
+                <div key={index}>
+                     <input type="text" />
+               
+                <img key={index} src={url} style={{width: "250px "}} />
+                </div>
+                
+            ))}
         </div>
     )
 
