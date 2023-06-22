c2914657309589d1b32b06feaeed6aa5d3cacb11
vinitha <vinitha.nec@gmail.com>
2023-06-21 22:50:34 -0700

    Complete adding custom message to each image and stored as a dictionary with message as value and url as key

--- frontend/src/components/features/Picture.jsx
+++ frontend/src/components/features/Picture.jsx
@@ -5,47 +5,69 @@ import { useNavigate } from 'react-router-dom';
 
 
 
-function Picture(props){
-   const cloudinaryRef = useRef();
-   const widgetRef = useRef();
-//    let uploadedUrl;
+function Picture(props) {
+    const cloudinaryRef = useRef();
+    const widgetRef = useRef();
+    //    let uploadedUrl;
 
-   const [showImage, setShowImage] = useState("");
-   const [imageUrls, setImageUrls] = useState([]);
+    const [showImage, setShowImage] = useState("");
+    const [imageUrls, setImageUrls] = useState([]);
+    const [messageAndUrl, setMessageAndUrl] = useState({});
+    //     key: url
+    // value: message
+    useEffect(cloudinaryValue, [])
 
-   useEffect(cloudinaryValue, [])
-
-   function cloudinaryValue(){
+    function cloudinaryValue(props) {
         cloudinaryRef.current = window.cloudinary;
         widgetRef.current = cloudinaryRef.current.createUploadWidget({
-        cloudName:'journalpictures',
-        uploadPreset:'k2hycxis'
-        }, function(error,result) {
-                // console.log(result);
-                const url = result.info.url;
-                console.log(url);
-                if (url) {
-                    setShowImage(url);
-                    // uploadedUrl = url;
-                    // setstageparameters(previousstatevalue)
-                    setImageUrls(imageUrls => [...imageUrls, url])
-                    // setImageUrls(prevUrls => [...prevUrls, url]);
-                    // setImageUrls((prevUrls) => { return [...prevUrls, url] });
-                    // setImageUrls(preurls);
-                    console.log(showImage)};
-                
+            cloudName: 'journalpictures',
+            uploadPreset: 'k2hycxis'
+        }, function (error, result) {
+            // console.log(result);
+            const url = result.info.url;
+            console.log(url);
+            if (url) {
+                setShowImage(url);
+                // uploadedUrl = url;
+                // setstageparameters(previousstatevalue)
+                setImageUrls(imageUrls => [...imageUrls, url])
+                // setImageUrls(prevUrls => [...prevUrls, url]);
+                // setImageUrls((prevUrls) => { return [...prevUrls, url] });
+                // setImageUrls(preurls);
+                console.log(showImage)
+            };
+
         });
     }
-  
+
     // function preurls(urls){
 
     //     return [...urls, uploadedUrl];
     // }
-    function displayImage(){
-            widgetRef.current.open()
-        }
+    function displayImage() {
+        widgetRef.current.open()
+    }
 
-    return(
+    function dic_update(event) {
+        let message = event.target.value;
+        // console.log(message);
+        // console.log(event.target.getAttribute("data-url"));
+        let image_url = event.target.getAttribute("data-url");
+        // messageAndUrl.push({key: message, value:image_url});
+        messageAndUrl[image_url] = message;
+        setMessageAndUrl(messageAndUrl);
+        console.log(messageAndUrl)
+    };
+    
+
+    
+    // function dic_update_v2(event, url) {
+    //     let message = event.target.value;
+    //     console.log(message);
+    //     console.log(url);
+    // };
+
+    return (
         <div>
             <p>This is where you will upload pictures</p>
             {/* <input type="file" name="file"/> */}
@@ -53,14 +75,14 @@ function Picture(props){
             {/* <button onClick={onButtonClick}></button> */}
             {/* <button type="submit">Upload</button> */}
             <button onClick={displayImage}>Cloudinary</button>
-            <div style={{ marginLeft: "50px", marginTop: "50px"}}/>
+            <div style={{ marginLeft: "50px", marginTop: "50px" }} />
             {imageUrls.map((url, index) => (
                 <div key={index}>
-                     <input type="text" />
-               
-                <img key={index} src={url} style={{width: "250px "}} />
+                    <input data-url={url} type="text" onChange={dic_update} />
+                    {/* <input type="text" onChange={(event) => {dic_update_v2(event, url)}} /> */}
+
+                    <img key={index} src={url} style={{ width: "250px " }} />
                 </div>
-                
             ))}
         </div>
     )
